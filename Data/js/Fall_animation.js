1d7a31a114c969feb9c749d99eb8a8c80ef44b69
LaythZubaidi <laythh.zubaidi@gmail.com>
2022-08-12 16:35:26 -0700

    Add files via upload

---
+++ Data/js/Fall_animation.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#Fall_animation').scianimator({
+	    'images': ['images/Fall_animation1.png', 'images/Fall_animation2.png', 'images/Fall_animation3.png', 'images/Fall_animation4.png', 'images/Fall_animation5.png', 'images/Fall_animation6.png', 'images/Fall_animation7.png', 'images/Fall_animation8.png', 'images/Fall_animation9.png', 'images/Fall_animation10.png', 'images/Fall_animation11.png', 'images/Fall_animation12.png', 'images/Fall_animation13.png', 'images/Fall_animation14.png', 'images/Fall_animation15.png', 'images/Fall_animation16.png', 'images/Fall_animation17.png', 'images/Fall_animation18.png', 'images/Fall_animation19.png', 'images/Fall_animation20.png', 'images/Fall_animation21.png'],
+	    'width': 200,
+	    'delay': 500,
+	    'loopMode': 'loop'
+	});
+	$('#Fall_animation').scianimator('play');
+    });
+})(jQuery);
